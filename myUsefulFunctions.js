f4392e133b59674ddede035345b9e64871af0768
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-01 15:59:06 -0300

    downloading inside promised

--- myUsefulFunctions.js
+++ myUsefulFunctions.js
@@ -1,13 +1,13 @@
 const sampl = require('./driveApiSample');
-
-module.exports={
- downloadMyFile(google,oAuth2Client){
-    const drive = google.drive({ version: "v3", auth: oAuth2Client });
-    const idF = '1oFmgWIHeQYjwcyC2N0W8TwnBwFicI5B8';
-    sampl(idF,drive).then((dt)=>{     
-    console.log('A '+dt) }).catch((err)=> console.log('Mrr'+err))
-
-},
+async function downloadMyFile(google,oAuth2Client){
+    return new Promise((resolve,reject)=>{
+       const drive = google.drive({ version: "v3", auth: oAuth2Client });
+       const idF = '1oFmgWIHeQYjwcyC2N0W8TwnBwFicI5B8';
+       sampl(idF,drive).then((dt)=>{console.log('A '+dt) }
+       ).catch((err)=> console.log('Mrr'+err))
+    })    
+}
+module.exports={downloadMyFile ,
  readMyToken(fs,oAuth2Client){
         console.log('reading token');
         let myJson = fs.readFileSync('./meutoken.json');
