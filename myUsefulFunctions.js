4760eb0b793b5f4e70428fe638a45eb2f77b2345
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-01 16:07:46 -0300

    organizing: down promise

--- myUsefulFunctions.js
+++ myUsefulFunctions.js
@@ -1,10 +1,17 @@
 const sampl = require('./driveApiSample');
 async function downloadMyFile(google,oAuth2Client){
     return new Promise((resolve,reject)=>{
-       const drive = google.drive({ version: "v3", auth: oAuth2Client });
-       const idF = '1oFmgWIHeQYjwcyC2N0W8TwnBwFicI5B8';
-       sampl(idF,drive).then((dt)=>{console.log('A '+dt) }
-       ).catch((err)=> console.log('Mrr'+err))
+       try{
+        const idF = '1oFmgWIHeQYjwcyC2N0W8TwnBwFicI5B8';
+        const drive = google.drive({ version: "v3", auth: oAuth2Client });
+        sampl(idF,drive).then((dt)=>{console.log('A '+dt) }
+        ).catch((err)=> console.log('Mrr'+err))
+       }catch(err){
+            reject(err);
+       }
+
+       
+
     })    
 }
 module.exports={downloadMyFile ,
