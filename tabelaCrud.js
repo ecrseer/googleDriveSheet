dbea16b4dc4466a4ed11491fd2513a33c01f541b
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-02 18:36:30 -0300

    organizing functions

--- tabelaCrud.js
+++ tabelaCrud.js
@@ -14,26 +14,23 @@ var readFilePromise = function(file) {
 /*(array)=> array.length==4*/
 function filtro(arr){ return arr.length==4;}
 
+async function pesquisar(requisicao,response){
+    console.log('ereto'+requisicao.body.nomezin);
+    const workSheetsFromFile = xlsx.parse(`${__dirname}/armazen/tabela.xlsx`),
+        nomezin = requisicao.body.nomezin,
+        planilha = workSheetsFromFile[1].data,
+        arraysBuscados = planilha.filter(array=>array.some(stri=>stri==nomezin))
+    
+    
+    let placeb = { meuArray:arraysBuscados};
+    console.log(arraysBuscados ) ;
+    const copy = await placeb;
 
+    return response.json(copy);
+}
 
 
 
 module.exports={
-    async pesquisar(requisicao,response){
-        console.log('ereto'+requisicao.body.nomezin);
-        const workSheetsFromFile = xlsx.parse(`${__dirname}/armazen/tabela.xlsx`),
-            nomezin = requisicao.body.nomezin,
-            planilha = workSheetsFromFile[1].data,
-            arraysBuscados = planilha.filter(array=>array.some(stri=>stri==nomezin))
-        
-        
-        let placeb = { meuArray:arraysBuscados};
-        console.log(arraysBuscados ) ;
-        const copy = await placeb;
-
-        return response.json(copy);
-    },
-    async pesquisar2(requisicao,resposta){
-        console.log('ereto'+requisicao.body.nomezin);
-    }
-}
+    pesquisar}
+    
