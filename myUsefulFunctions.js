45656d8138f7c64e3ac605e1d58368488bd05e6b
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-03 02:51:09 -0300

    Update myUsefulFunctions.js

--- myUsefulFunctions.js
+++ myUsefulFunctions.js
@@ -3,8 +3,8 @@ async function downloadMyFile(google,oAuth2Client){
     return new Promise((resolve,reject)=>{
        try{
         const idF = '194t1ptGWmjV1uDGqepXUZhiEUAgnr8D9';
-        const drive = google.drive({ version: "v3", auth: oAuth2Client });
-        const path =  sampl(idF,drive).then((dt)=>{console.log('A '+dt) }
+        const drivest = google.drive({ version: "v3", auth: oAuth2Client });
+        const path =  sampl(idF,drivest).then((dt)=>{console.log('A '+dt) }
         ).catch((err)=> console.log('Erro no downloadMyFile: '+err))
         resolve(path);
        }catch(err){
